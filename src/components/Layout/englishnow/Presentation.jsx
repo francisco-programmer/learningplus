0f44c63ea5c1b79cf5be1a40b0ae2c2dcd08bf7e
francisco-programmer <franciscopalenciaa@gmail.com>
2023-06-23 13:29:58 -0500

    se agrega redireccionador vercel

--- src/components/Layout/englishnow/Presentation.jsx
+++ src/components/Layout/englishnow/Presentation.jsx
@@ -57,4 +57,4 @@ const Presentation = () => {
   );
 }
 
-export default Presentation
+export default Presentation 
--- src/main.jsx
+++ src/main.jsx
@@ -14,7 +14,7 @@ const router = createBrowserRouter([
     element: <App />,
   }, 
 {
-  path:"EnglishNow",
+  path:"englishnow",
   element: <EnglishNow />,
 },
 ]);
